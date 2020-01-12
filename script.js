77e1d3aa997d27f2ae022b177be36714fc54eeaf
imane <benbenimane@hotmail.com>
2020-01-12 14:53:29 +0100

    getting the avergae temp and other stuff

--- script.js
+++ script.js
@@ -49,50 +49,52 @@ function GetCity(){
 
 
 
-               let i;
-               //console.log(temp,tempFeel,mintemp,maxtemp);
 
-               for (i = 0; i < data.list.length; i++) {
-                   let templist = Math.floor(data.list[i].main.temp);
-                   //console.log(templist);
-                   temp3hours.push(templist);
-                   //console.log(templist);
+        //loop to get temp list every 3 hours
+        let i;
+        for (i = 0; i < data.list.length; i++) {
+             let templist = Math.floor(data.list[i].main.temp);
+             //console.log(templist);
+             temp3hours.push(templist);
+             //console.log(templist);
+            let icon = data.list[i].weather[i].icon;
+            console.log(icon);
+        }
 
-               }
-
-               // console.log(temp3hours.length);
-               //console.log(temp3hours);
-
-               // console.log( temp3hours.splice(1,8));
-
-
-               let day1 = [temp3hours.slice(0, 8)];
 
         // Getting sum of numbers
+        function average(array) {
+            return Math.round(array.reduce((a, b) => a + b) / array.length);
+        }
 
+        //slice array of temp per day and get average temp
+        let day1 = temp3hours.slice(0, 8);
+        day1 = average(day1);
 
+        let day2 = temp3hours.slice(8,16);
+        day2 = average(day2);
 
+        let day3 = temp3hours.slice(16,24);
+        day3 = average(day3);
 
+        let day4 = temp3hours.slice(24,32);
+        day4 = average(day4);
 
-               let day2 = [ temp3hours.slice(8,16)];
+        let day5 = temp3hours.slice(32,50);
+        day5 = average(day5);
 
-               let day3 = [ temp3hours.slice(16,24)];
+        console.log(day1, day2, day3, day4,day5);
 
-               let day4 = [ temp3hours.slice(24,32)];
-
-               let day5 = [ temp3hours.slice(32,50)];
-
-console.log(day1, day2, day3, day4,day5);
-
+        //assign API data to DOM elements
         let weather = document.getElementById('weather');
         weather.innerHTML = getweather;
 
         let temperature = document.getElementById('temp');
         temperature.innerHTML = `feels like ${tempFeel}&degC rigth now`;
 
-        let degrees = document.querySelector('.degrees');
+        let averagetemp = document.querySelector('.degrees');
 
-        degrees.innerHTML= `${temp}&degC`
+        averagetemp.innerHTML= `${day1}&degC`
 
     }
     console.log(Getdata());
